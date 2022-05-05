0fd76058c6fa8d43a39b55b55408443c68c627be
s5307656 <100590984+s5307656@users.noreply.github.com>
2022-05-05 14:44:59 +0200

    Create lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "lib.h"
+float max (float array [], int d)
+{
+    float m = array [0];
+    for (int i =1; i<d; i++){
+        if (array[i]>m){
+            m= array [i];
+        }
+    }
+    return m;
+}
