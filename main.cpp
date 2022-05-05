f978f0a05f17e43f1c5dda1d982d9ddc64aa622a
s5307656 <100590984+s5307656@users.noreply.github.com>
2022-05-05 14:46:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int d=0;
+    cin >> d;
+    float  m[d];
+    for (int i=0;i<d; i++) {
+        cin >> m[i];
+    }
+    cout<< max (m,d);
+
+    return 0;
 }
